ce2273cc0a8c47e8e12c31521a63e9a787da9290
OlaStandard <OlaStandard@Olas-MacBook-Air.local>
2018-10-18 10:53:35 +0200

    main structure of project and librarys: buttercnife, recyclerview

---
+++ app/src/main/java/com/example/olastandard/appforseniors/ManuActivity.java
@@ -0,0 +1,13 @@
+package com.example.olastandard.appforseniors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManuActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu);
+    }
+}
---
+++ app/src/main/java/com/example/olastandard/appforseniors/Objects/MenuItem.java
@@ -0,0 +1,14 @@
+package com.example.olastandard.appforseniors.Objects;
+
+import android.widget.ImageView;
+
+public class MenuItem {
+    public ImageView icon;
+    public String text;
+
+    public MenuItem(ImageView icon, String text) {
+        this.icon = icon;
+        this.text = text;
+    }
+
+}
