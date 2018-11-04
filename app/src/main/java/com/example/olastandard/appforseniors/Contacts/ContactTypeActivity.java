a0fd76362a1c2d0e874ec13cd670594a37b0e534
izasal <iza.saltarius@gmail.com>
2018-11-04 20:31:26 +0100

    open default view to write and call

--- app/src/main/java/com/example/olastandard/appforseniors/Contacts/ContactTypeActivity.java
+++ app/src/main/java/com/example/olastandard/appforseniors/Contacts/ContactTypeActivity.java
@@ -1,10 +1,12 @@
 package com.example.olastandard.appforseniors.Contacts;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.example.olastandard.appforseniors.MainActivity;
 import com.example.olastandard.appforseniors.R;
@@ -16,13 +18,35 @@ public class ContactTypeActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         initAddlayout(R.layout.activity_contact_type);
         initToolbar();
-        System.out.println("bla bla");
-        Button app_layer = (Button) findViewById (R.id.go_to_contact);
-        app_layer.setOnClickListener(new View.OnClickListener() {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE},1);
+        }
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        Button go_to_contact_list = (Button) findViewById (R.id.go_to_contact);
+        Button call_at = (Button) findViewById (R.id.call_at_number);
+
+        go_to_contact_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(v.getContext(), ContactListActivity.class));
 
+            }
+        });
+        call_at.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                if (ActivityCompat.checkSelfPermission(v.getContext(),
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
             }
         });
     }
