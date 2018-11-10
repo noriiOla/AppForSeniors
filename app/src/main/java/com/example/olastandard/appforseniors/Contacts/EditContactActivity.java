8c5739c17fe06e762ec1d38ce6eaa33e63b598e6
izasa <iza.saltarius@gmail.com>
2018-11-10 20:50:46 +0100

    edit contact

--- app/src/main/java/com/example/olastandard/appforseniors/Contacts/EditContactActivity.java
+++ app/src/main/java/com/example/olastandard/appforseniors/Contacts/EditContactActivity.java
@@ -2,19 +2,21 @@ package com.example.olastandard.appforseniors.Contacts;
 
 import android.Manifest;
 import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.olastandard.appforseniors.MainActivity;
 import com.example.olastandard.appforseniors.Objects.ContactData;
-import com.example.olastandard.appforseniors.Objects.PersonSmsData;
 import com.example.olastandard.appforseniors.R;
 
 import java.util.ArrayList;
@@ -46,12 +48,12 @@ public class EditContactActivity extends MainActivity {
                 Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_CONTACTS},1);
 
-        }if (ActivityCompat.checkSelfPermission(this,
+        }
+        if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},2);
         }
         contactData = (ContactData) getIntent().getSerializableExtra("contactData");
-
         contactNameInput.setText(contactData.getNameOfPersion());
         contactNumberInput.setText(contactData.getNumebrOfPerson());
 
@@ -61,7 +63,7 @@ public class EditContactActivity extends MainActivity {
                 //TODO: walidacja
                 String contactName = contactNameInput.getText().toString();
                 String contactNumber = contactNumberInput.getText().toString();
-                updateContactList(v.getContext(), contactName,contactNumber);
+                updateNameAndNumber(v.getContext(), contactData.getNumebrOfPerson(), contactName,contactNumber);
                 startActivity(new Intent(v.getContext(), ContactListActivity.class));
             }
         });
@@ -73,28 +75,84 @@ public class EditContactActivity extends MainActivity {
         setTitle(R.string.new_number);
     }
 
-    public static boolean updateContactList(Context context, String name, String newPhoneNumber){
-        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
-        System.out.println("-------EDIT----------" + name);
-        String where = ContactsContract.Data.DISPLAY_NAME + " = ? AND " +
-                ContactsContract.Data.MIMETYPE + " = ? AND " +
-                String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE) + " = ? ";
+    private final static String[] DATA_COLS = {
 
-        String[] params = new String[] {name,
+            ContactsContract.Data.MIMETYPE,
+            ContactsContract.Data.DATA1,//phone number
+            ContactsContract.Data.CONTACT_ID
+    };
+
+    public static boolean updateNameAndNumber(final Context context, String number, String newName, String newNumber) {
+        if (context == null || number == null || number.trim().isEmpty()) return false;
+        if (newNumber != null && newNumber.trim().isEmpty()) newNumber = null;
+        if (newNumber == null) return false;
+
+        String contactId = getContactId(context, number);
+        if (contactId == null) return false;
+
+        //selection for name
+        String where = String.format(
+                "%s = '%s' AND %s = ?",
+                DATA_COLS[0], //mimetype
+                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE,
+                DATA_COLS[2]/*contactId*/);
+
+        String[] args = {contactId};
+
+        ArrayList<ContentProviderOperation> operations = new ArrayList<>();
+
+        operations.add(
+                ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                        .withSelection(where, args)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, newName)
+                        .build()
+        );
+
+        //change selection for number
+        where = String.format(
+                "%s = '%s' AND %s = ?",
+                DATA_COLS[0],//mimetype
                 ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE,
-                String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE_HOME)};
-        ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
-                .withSelection(where, params)
-                .withValue(ContactsContract.CommonDataKinds.Phone.DATA, newPhoneNumber)
-                .build());
-        try {
-            context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-            System.out.println("successful update");
-            return true;
+                DATA_COLS[1]/*number*/);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        //change args for number
+        args[0] = number;
+
+        operations.add(
+                ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                        .withSelection(where, args)
+                        .withValue(DATA_COLS[1]/*number*/, newNumber)
+                        .build()
+        );
+
+        try {
+            ContentProviderResult[] results = context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, operations);
+            return true;
         }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public static String getContactId(Context context, String number) {
+
+        if (context == null) return null;
+
+        Cursor cursor = context.getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.CONTACT_ID, ContactsContract.CommonDataKinds.Phone.NUMBER},
+                ContactsContract.CommonDataKinds.Phone.NUMBER + "=?",
+                new String[]{number},
+                null
+        );
+
+        if (cursor == null || cursor.getCount() == 0) return null;
+
+        cursor.moveToFirst();
+        String id = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+        cursor.close();
+        return id;
     }
 }
