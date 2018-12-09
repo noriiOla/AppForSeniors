508294795825c905bfccfb280f1cca0bafed2539
Aleksandra Plaszczyk <220880@student.pwr.edu.pl>
2018-12-09 09:43:21 +0100

    permissions repair

--- app/src/main/java/com/example/olastandard/appforseniors/smsActivitys/MessagerListActivity.java
+++ app/src/main/java/com/example/olastandard/appforseniors/smsActivitys/MessagerListActivity.java
@@ -15,7 +15,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import com.example.olastandard.appforseniors.ExampleActivity;
 import com.example.olastandard.appforseniors.MainActivity;
@@ -49,7 +48,6 @@ public class MessagerListActivity extends MainActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    private static final int SMS_PERMISSION_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,57 +61,25 @@ public class MessagerListActivity extends MainActivity {
         buttonDelete.setBackground(getResources().getDrawable(R.drawable.button_shape_green));
         buttonSelect.setBackground(getResources().getDrawable(R.drawable.button_shape_white));
 
-        if (!hasReadSmsPermission()) {
-            showRequestPermissionsInfoAlertDialog();
-        }else {
+        if (hasReadSmsPermission()) {
             smsHelper = new SmsHelper(getApplicationContext(), this);
             List<PersonSmsData> listaSmsow = smsHelper.actualizeListOfSms();
             initRecyclerView(listaSmsow);
         }
+
     }
 
     private boolean hasReadSmsPermission() {
         return ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestReadAndSendSmsPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
-            Toast.makeText(this,"shouldShowRequestPermissionRationale(), no permission requested", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS},
-                SMS_PERMISSION_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    smsHelper = new SmsHelper(getApplicationContext(), this);
-                    List<PersonSmsData> listaSmsow = smsHelper.actualizeListOfSms();
-                    initRecyclerView(listaSmsow);
-
-                } else {
-                    buttonSelect.setVisibility(View.GONE);
-                    buttonDelete.setVisibility(View.GONE);
-                    this._toolbarSaveButton.setVisibility(View.GONE);
-
-                }
-                return;
-            }
-        }
+                        Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
     }
 
     public void addListeners() {
         this._toolbarSaveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 if (((SmsPersonListAdapter) mAdapter).lastSelectedItem == -1) {
                     new PushDialogManager().showDialogWithOkButton(MessagerListActivity.this, "Żadna wiadomość nie została wybrana", new PushDialogButtonsOkInterface() {
                         @Override
@@ -122,7 +88,6 @@ public class MessagerListActivity extends MainActivity {
                         }
                     });
                 } else {
-
                     (new PushDialogManager()).showDialogWithYesNoButtons(MessagerListActivity.this, "Czy na pewno chcesz usunąć wszystkie wiadomości od zaznaczonego kontaktu?", new PushDialogButtonsYesNoInterface() {
 
                         @Override
@@ -181,15 +146,6 @@ public class MessagerListActivity extends MainActivity {
         ((SmsPersonListAdapter)mAdapter).lastSelectedItem = index;
         mAdapter.notifyDataSetChanged();
         changeButtonsColor();
-    }
-
-    private void showRequestPermissionsInfoAlertDialog() {
-        new PushDialogManager().showDialogWithOkButton(this, getResources().getString(R.string.sms_persmission), new PushDialogButtonsOkInterface() {
-            @Override
-            public void onOkButtonTap() {
-                requestReadAndSendSmsPermission();
-            }
-        });
     }
 }
 
