3b7aacbb53ccff2a6a6e5b5219ee683a59fde293
Edyta <edyta9595@wp.pl>
2018-12-11 17:30:39 +0100

    repairalarm6

--- app/src/main/java/com/example/olastandard/appforseniors/AlarmClock/AlarmActivity.java
+++ app/src/main/java/com/example/olastandard/appforseniors/AlarmClock/AlarmActivity.java
@@ -60,12 +60,15 @@ public class AlarmActivity  extends MainActivity {
         System.out.println("----> alarm activity");
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         Intent myIntent = new Intent(AlarmActivity.this, AlarmReceiver.class);
-        appIntent = PendingIntent.getBroadcast(this,(int) newTome2 , myIntent,PendingIntent.FLAG_ONE_SHOT);
-        if(c.getTimeInMillis() <= now.getTimeInMillis())
-            _alarm = calendar.getTimeInMillis() + (AlarmManager.INTERVAL_DAY+1);
-        else
-            _alarm = calendar.getTimeInMillis();
 
+        Calendar now = Calendar.getInstance();
+        long _alarm=0;
+        if(c.getTimeInMillis() <= now.getTimeInMillis())
+            _alarm = c.getTimeInMillis() + (AlarmManager.INTERVAL_DAY+1);
+        else
+            _alarm = c.getTimeInMillis();
+        appIntent = PendingIntent.getBroadcast(this,(int) newTome2 , myIntent,PendingIntent.FLAG_ONE_SHOT);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, _alarm , appIntent);
 
 
 
