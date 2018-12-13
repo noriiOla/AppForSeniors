e1c2f79bfc3c966a144f9a034f0e03e25fdb86ee
izasa <iza.saltarius@gmail.com>
2018-12-13 15:26:17 +0100

    GUI improvement - add notes

--- app/src/main/java/com/example/olastandard/appforseniors/VoiceNotes/AddVoiceNoteActivity.java
+++ app/src/main/java/com/example/olastandard/appforseniors/VoiceNotes/AddVoiceNoteActivity.java
@@ -1,24 +1,16 @@
 package com.example.olastandard.appforseniors.VoiceNotes;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.olastandard.appforseniors.Contacts.ContactListAdapter;
-import com.example.olastandard.appforseniors.Contacts.EditContactActivity;
 import com.example.olastandard.appforseniors.MainActivity;
-import com.example.olastandard.appforseniors.Objects.ContactData;
 import com.example.olastandard.appforseniors.PushDIalog.PushDialogButtonsOkInterface;
 import com.example.olastandard.appforseniors.PushDIalog.PushDialogManager;
 import com.example.olastandard.appforseniors.R;
-import com.example.olastandard.appforseniors.smsActivitys.NewSmsActivity;
-import com.google.android.gms.common.data.DataBufferUtils;
-
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 import butterknife.BindView;
@@ -27,12 +19,17 @@ import butterknife.OnClick;
 
 public class AddVoiceNoteActivity  extends MainActivity {
 
-    @BindView(R.id.start_recording)
-    public Button buttonStartRecording;
+    @BindView(R.id.note_title_input)
+    public EditText placeTitle;
 
-    @BindView(R.id.stop_recording)
-    public Button buttonStopRecording;
-    VoiceNotesManager voiceNotesManager;
+    @BindView(R.id.record_action)
+    public Button buttonRecordAction;
+
+    private VoiceNotesManager voiceNotesManager;
+    private boolean isRecording = false;
+    private boolean isNoteExisting = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,31 +38,48 @@ public class AddVoiceNoteActivity  extends MainActivity {
         initToolbar();
         voiceNotesManager = VoiceNotesManager.getInstance();
         changeButtonsColor();
+
+        this._toolbarSaveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String titleText = placeTitle.getText().toString();
+                if (titleText.isEmpty()) {
+                    showDialogBox("Wpisz tytuł");
+                } else {
+                    String title = titleText.toString();
+                    System.out.println("nagranie: " + title);
+                    voiceNotesManager.saveRecord(title);
+
+                    (new PushDialogManager()).showDialogWithOkButton(AddVoiceNoteActivity.this,
+                            "zapisano nagranie: " + title , new PushDialogButtonsOkInterface(){
+                                @Override
+                                public void onOkButtonTap() {
+                                }
+                            });
+                }
+            }
+        });
     }
 
-    @OnClick({R.id.start_recording})
-    public void startRecordingNote() {
-        voiceNotesManager.startRecording();
-        Toast.makeText(getApplicationContext(),"Pocatek nagrywania" ,Toast.LENGTH_LONG).show();
-    }
-
-    @OnClick({R.id.stop_recording})
-    public void stopRecordingNote() {
-        voiceNotesManager.stopRecording();
-        Toast.makeText(getApplicationContext(),"Koniec nagrywania" ,Toast.LENGTH_LONG).show();
-    }
-
-    @OnClick({R.id.save_record})
-    public void saveRecordedNote() {
-        System.out.println();
-        String name = generateRandomString();
-        voiceNotesManager.saveRecord(name);
-        (new PushDialogManager()).showDialogWithOkButton(this,
-                "zapisano nagranie: " + name , new PushDialogButtonsOkInterface(){
+    private void showDialogBox(String text) {
+        new PushDialogManager().showDialogWithOkButton(this, text, new PushDialogButtonsOkInterface() {
             @Override
             public void onOkButtonTap() {
             }
-            });
+        });
+    }
+
+    @OnClick({R.id.record_action})
+    public void handleActionOnNotes() {
+        if(isRecording){
+            voiceNotesManager.stopRecording();
+            isRecording = false;
+            isNoteExisting = true;
+            buttonRecordAction.setText(R.string.start_recording);
+        }else{
+            isRecording = true;
+            voiceNotesManager.startRecording();
+            buttonRecordAction.setText(R.string.stop_recording);
+        }
     }
 
     private void initToolbar() {
@@ -90,8 +104,6 @@ public class AddVoiceNoteActivity  extends MainActivity {
     }
 
     private void changeButtonsColor() {
-        buttonStartRecording.setBackground(getResources().getDrawable(R.drawable.button_shape_red));
-        buttonStopRecording.setBackground(getResources().getDrawable(R.drawable.button_shape_green));
-        //buttonPlayStop.setBackground(getResources().getDrawable(R.drawable.button_shape_green));
+       buttonRecordAction.setBackground(getResources().getDrawable(R.drawable.button_shape_green));
     }
 }
